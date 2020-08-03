80a3aa85a0fc12ec18ee706f4d06d5f096bede7b
Kordell Parrish <parrishkordell@gmail.com>
2020-08-03 00:01:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
 phrase = "Hello there, and how are you?"
 def reverse_each_word(phrase)
   array = phrase.split
-  ary = []
+  array2 = []
   array.each do |stuff|
-    ary << stuff.reverse
+    array2 << stuff.reverse
   end
+  ary = phrase.split
+  ary2 = ary.collect do |stuff|
+    stuff.reverse
   ary.join(" ")
 end
